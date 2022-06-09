41aa1b8b4e2605ea794a9889c3e29df4561048d7
Júlio César <101370931+JulioBastianelle@users.noreply.github.com>
2022-06-09 19:14:17 -0300

    Update Principal.java

--- _6_topicos_avancados/src/_6_3_desafio_classe_java_lang_math/Principal.java
+++ _6_topicos_avancados/src/_6_3_desafio_classe_java_lang_math/Principal.java
@@ -14,16 +14,14 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-
         System.out.println("Quantos jogos?");
         int quantidadeJogos = entrada.nextInt();
-        System.out.println("----------------------------");
-
+        System.out.println("=============");
         for (int i = 1; i<= quantidadeJogos; i++){
             Jogo jogo = new Jogo();
             jogo.sortear();
             jogo.exibirNumeros();
-            System.out.println("----------------------------");
+            System.out.println("============");
         }
         System.out.println("$$$ Boa sorte! $$$");
     }
